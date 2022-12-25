553e0b7c21027a03d6c6e11214f425c80a8acb16
Saman Mahdanian <saman@mahdanian.xyz>
2022-12-26 01:01:39 +0330

    logs added to sender

--- sender/main.go
+++ sender/main.go
@@ -4,23 +4,28 @@
 package sender
 
 import (
-	"log"
 	"time"
 
 	"github.com/SamMHD/simple-broker/util"
+
+	"github.com/rs/zerolog/log"
 )
 
-const TPS = 1
+const TPS = 10000
 const testDuration = 10
 
 // StartSendProcedure will send messages at a rate of TPS for testDuration seconds
+
 func StartSendProcedure(config util.Config) {
 	// send messages at a rate of TPS for testDuration seconds
+	i := 0
 	for start := time.Now(); time.Since(start) < time.Second*testDuration; {
 		// send a message
+		log.Info().Msgf("sending message %d", i)
+		i++
 		err := sendMessage(config)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal().Msgf("%s", err)
 		}
 
 		// sleep for 1/TPS seconds
